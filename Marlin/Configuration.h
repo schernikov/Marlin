864d8657d21c3c47602b5ac3212b2696c7031449
Sergey Chernikov <schernikov@gmail.com>
2015-10-24 15:11:24 -0700

    RRP Huxley + Sanguinololu, model Feb 2012; bed adjustments

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -339,11 +339,11 @@ const bool Z_MAX_ENDSTOP_INVERTING = false; // set to true to invert the logic o
 #define max_software_endstops true  // If true, axis won't move to coordinates greater than the defined lengths below.
 
 // Travel limits after homing
-#define X_MAX_POS 155
-#define X_MIN_POS 0
-#define Y_MAX_POS 150
-#define Y_MIN_POS 0
-#define Z_MAX_POS 90
+#define X_MAX_POS 140
+#define X_MIN_POS -2
+#define Y_MAX_POS 140
+#define Y_MIN_POS -7
+#define Z_MAX_POS 70
 #define Z_MIN_POS 0
 
 #define X_MAX_LENGTH (X_MAX_POS - X_MIN_POS)
